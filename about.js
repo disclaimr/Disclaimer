a9bb2c9ee23c4372731284e662673ac9d034ba90
disclaimr <disclaimer0001@gmail.com>
2026-08-23 22:08:56 -0600

    combined stylesheets, connected disclaimer page , added spacebar skip

--- about.js
+++ about.js
@@ -1,4 +1,10 @@
 /* CODE FOR THE TYPEWRITER ANIMATIONS */
+document.getElementById("title").style.display = "none";
+document.getElementById("tabs").style.display = "none";
+document.getElementById("body").style.display = "none";
+
+
+
 const OutputDiv = document.getElementById('typer')
 
 const txt = `Trevor Harms, AKA Disclaimer, is a sporadic idealist whose actions tie most closely to environmental justice 
@@ -14,16 +20,27 @@ span.classList.add('cursor'); /*adds the class cursor to the span object*/
 p.appendChild(span); /*span is now a child of the p object*/
 OutputDiv.appendChild(p); /* p (and therefore also span) is now a child of OutputDiv*/
 
+let speed = 40;
 let i = 0
-const IntervalId = setInterval(function() {
+let IntervalId = setInterval(typeWriter, speed);
+
+function typeWriter() {
     OutputDiv.textContent += txt[i];
     i++;
 
     if (i === txt.length) {
         clearInterval(IntervalId);
-        document.getElementById("top").style.top = "355px";
         document.getElementById("title").style.display = "flex";
         document.getElementById("tabs").style.display = "flex";
-        document.getElementsByClassName("main").style.display = "block";
+        document.getElementById("body").style.display = "block";
+        document.getElementById("skip").style.display = "none";
     }
-}, 40)
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === "Space") {
+        clearInterval(IntervalId);
+        speed = 1;
+        IntervalId = setInterval(typeWriter, speed);
+    }
+});
